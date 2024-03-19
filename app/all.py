804957c75a6bdfa950a81b42bfe515deaa9c8543
Prerak Lodha <prerakl123@gmail.com>
2024-03-20 00:36:48 +0530

    Update endpoints: add b64 image retrieval, add "/analysis_settings" and "/people_config"

--- app/all.py
+++ app/all.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, session, make_response, redirect, url_for
+from flask import Blueprint, request, render_template, session, make_response, redirect, url_for, send_file, jsonify
 
 from app.factory import sql_db, emotion_analysis_task_manager
 from app.forms import ConfigureAnalysisSettingsForm, ConfigurePeopleForm
@@ -28,22 +28,16 @@ def upload():
 
     :return:
         GET  : rendered HTML template `upload.html`.
-        POST : (response message, response integer).
+        POST : (response message, response integer) | redirect.
     """
-    cas_form = ConfigureAnalysisSettingsForm()
-    cp_form = ConfigurePeopleForm()
-
-    if cas_form.validate_on_submit():
-        return redirect(url_for("all.analysis_settings"))
-
-    if cp_form.validate_on_submit():
-        return redirect(url_for("all.people_config"))
 
     if request.method == 'POST':
+        print("SAVE FILE")
         file = request.files['file']
         video_file = VideoParser(file)
         ea = EmotionAnalyzer(video_file)
         emotion_analysis_task_manager.add_task(ea)
+        print(video_file.video_dir)
 
         session.__setitem__("latest_upload", video_file.file_hash)
 
@@ -61,7 +55,44 @@ def upload():
         return make_response(("File Uploaded Successfully", 200))
 
     elif request.method == 'GET':
-        return render_template('upload.html', cas_form=cas_form, cp_form=cp_form)
+        return render_template('upload.html')
+
+
+@bp.route('/analysis_settings', methods=['GET', 'POST'])
+def analysis_settings():
+    return render_template("settings_analysis.html")
+
+
+@bp.route('/people_config', methods=['GET', 'POST'])
+def people_config():
+    try:
+        start = int(request.args.get('start'))
+        end = int(request.args.get('end'))
+    except TypeError:
+        start = 0
+        end = 10
+
+    tasks = emotion_analysis_task_manager.get_tasks(start, end)
+    print(tasks, end="\n")
+
+    return render_template(
+        "settings_people.html",
+        tasks=tasks
+    )
+
+
+@bp.route('/get_image', methods=['POST'])
+def get_image():
+    file_hash = request.args.get('file_hash')
+    img_index = int(request.args.get('img_index'))
+    img_b64 = VideoParser.get_image_from_hash_index(file_hash, img_index)
+    # return jsonify({"image":})
+    # return send_file(
+    #     img_io,
+    #     mimetype='image/jpeg',
+    #     as_attachment=False,
+    #     download_name='person_%s.jpg' % img_index
+    # )
 
 
 @bp.route('/progress', methods=['GET', 'POST'])
